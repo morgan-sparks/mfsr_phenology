f30210bc805def0da399a3b3cdba0429180ced52
Morgan.Sparks <morgan.sparks@morgans-mbp.boise.rmrs.fs.fed.us>
2025-03-12 14:35:33 -0600

    initial commit

---
+++ R/01_siegel_mfsr_temps.R
@@ -0,0 +1,17 @@
+# script to take full download from Siegel et al. 2023 and 
+# grab middle fork salmon river sites
+
+#source data: https://zenodo.org/records/8174951
+
+#libraries
+library(fs); library(stringr); library(purrr); library(readr)
+
+# file list of all files for salmon huc6 download
+huc_17062_files <- dir_ls("~/Downloads/st_pred_170602/")
+
+# put mfsr files in a df together
+mfsr_huc8 <- huc_17062_files |> 
+  str_subset(pattern = ("17060205|17060206")) |> # str subset on mfsr huc8
+  map_df(read_tsv, .id = "file", col_types = cols())
+
+write_rds(mfsr_huc8, "./data/siegel_temperature/siegel_mfsr.rds")
---
+++ R/02_combine_mfsr_spawn.R
@@ -0,0 +1,18 @@
+library(fs); library(stringr); library(purrr); library(readr); library(readxl)
+
+# list of spawn data from russ thurow
+spawn_files <- dir_ls("./data/russ_spawn/raw/")
+  
+# load and combine data into single object
+mfsr_spawn <- spawn_files[c(1:3,5)] |> # leave out 2004 (see below)
+  map_df(read_xls, .id = "file", sheet = "AllRch", range = cell_cols(c(1:9)))  |> # load in data, keep cols 1-9
+  select(-"file")
+
+# bring in 2004 data separately because it wants to load the easting as a character
+data_2004 <- read_xls("./data/russ_spawn/raw/04_ReddMonitor_AvgXY.xls", range = cell_cols(c(1:9)), sheet = "AllRch")
+data_2004$EASTING <- as.numeric(data_2004$EASTING) # change easting to a character
+
+# put all data into mfsr_spawn object
+mfsr_spawn <- bind_rows(mfsr_spawn, data_2004)
+
+write_rds(mfsr_spawn, "./data/russ_spawn/mfsr_spawn_combined.rds")
