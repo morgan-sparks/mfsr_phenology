fd7eacb455911cecfa230ac2591f5036c1b23a94
Morgan.Sparks <morgan.sparks@morgans-mbp.boise.rmrs.fs.fed.us>
2025-03-14 13:49:06 -0600

    clean up mess around

--- R/code_mess_around.R
+++ R/code_mess_around.R
@@ -2,20 +2,11 @@ library(tidyverse); library(ggridges)
 
 mfsr_spawn <- read.csv("./data/russ_spawn/mfsr_spawn_combined.csv")
 
-mfsr_spawn
 
-x <- as.character(mfsr_spawn$DATE)
-
-parse_date_time(na.omit(x), "%m%d%y")
-
-visdat::vis_dat(mfsr_spawn)
-
-visdat::vis_dat(data.frame(x))
 
 max(na.omit(mfsr_spawn$DATE))
 min(na.omit(mfsr_spawn$DATE))
 
-mdy(gsub('.*^','0',x))
 
 mfsr_spawn |> 
  drop_na(REACH) |> 
@@ -27,3 +18,14 @@ mfsr_spawn |>
   geom_density_ridges(scale = 1.1) +
   labs(x = "Day of Year", y = "Year") +
   theme_classic()
+
+mfsr_spawn |> 
+  drop_na(REACH) |> 
+  mutate(across(DATE, as.character)) |> 
+  mutate(date = mdy(gsub('.*^','0',DATE))) |>
+  mutate(year = year(date)) |> 
+  mutate(yday = yday(date)) |>
+  mutate(month = month(date)) |> 
+  filter(OBJ_CLASS %in% c("Redd", "REDD")) |> 
+  group_by(year) |> 
+  tally()
