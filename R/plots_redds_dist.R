bd1f0dac99afd730cc985304216ecc1611186684
bmait101 <Bryan.Maitland@usda.gov>
2025-03-19 16:00:28 -0600

    Add lemon facet wrap and re-render

--- R/plots_redds_dist.R
+++ R/plots_redds_dist.R
@@ -18,14 +18,40 @@ plot <-  mfsr_spawn_cleaned |>
   mutate(across(year, as.character)) |> 
   group_by(stream, year) |> 
   ggplot() +
+  # facet_wrap(~stream, scales = "free_y", ncol = 1) +
+  lemon::facet_rep_wrap(~stream, scales = "free_y", ncol = 1) + 
   geom_vline(xintercept = mfsr_all$median, color = "blue", linetype = "dashed") +
   geom_vline(xintercept = c(mfsr_all$percentile_95, mfsr_all$percentile_5), color = "purple", linetype = "dashed") +
   geom_density(aes(x = yday, y = after_stat(count), fill = year),  alpha = 0.5) +
   geom_density(data = mfsr_by_site, aes(x = yday, after_stat(count)), alpha = 0, linetype = "dashed") +
-  facet_wrap(~stream, scales = "free_y", ncol = 1) +
   scale_fill_brewer(palette = "Dark2", name = "Year") +
   labs(x = "Spawn Day of Year", y = "Count") +
   theme_classic() 
+plot
+
+# Save plot ====================================================================
+
+path <- here::here("plots", "redd_spawn_distributions")
+
+ggsave(
+  glue::glue("{path}.png"), 
+  plot, 
+  device = ragg::agg_png, 
+  height = 11,
+  width = 4,
+  res = 300,  
+  units = "in"
+) 
+
+ggsave(
+  glue::glue("{path}.pdf"), 
+  plot, 
+  device = cairo_pdf, 
+  height = 11,
+  width = 4,
+  units = "in"
+) 
+
 
 ggsave(here::here("plots", "redd_spawn_distributions.pdf"),
                   plot, 
