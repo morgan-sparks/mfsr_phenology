fb9d82d492800d726741ce64fb9b6e1bcef24884
Morgan.Sparks <morgan.sparks@morgans-mbp.boise.rmrs.fs.fed.us>
2025-03-19 08:28:05 -0600

    add basin summary to plots

--- R/plots_redds_dist.R
+++ R/plots_redds_dist.R
@@ -3,12 +3,14 @@ library(tidyverse); library(ggridges)
 mfsr_spawn_cleaned <- read_csv(here::here("data", "russ_spawn","mfsr_spawn_cleaned.csv"))
 
 mfsr_by_site <- mfsr_spawn_cleaned |> 
-  filter(stream != "Knapp" & stream != "Cape Horn") |> 
+  filter(stream != "Knapp" & stream != "Cape Horn" & year != 2001) |> 
   group_by(stream, yday) 
 
 mfsr_all <- mfsr_spawn_cleaned |> 
-  filter(stream != "Knapp" & stream != "Cape Horn") |> 
-  group_by(yday) 
+  filter(stream != "Knapp" & stream != "Cape Horn" & year != 2001) |> 
+  summarise(median = median(yday),
+            percentile_95 = quantile(yday,probs = 0.95),
+            percentile_5 = quantile(yday, probs = 0.05))
 
 
 plot <-  mfsr_spawn_cleaned |> 
@@ -16,6 +18,8 @@ plot <-  mfsr_spawn_cleaned |>
   mutate(across(year, as.character)) |> 
   group_by(stream, year) |> 
   ggplot() +
+  geom_vline(xintercept = mfsr_all$median, color = "blue", linetype = "dashed") +
+  geom_vline(xintercept = c(mfsr_all$percentile_95, mfsr_all$percentile_5), color = "purple", linetype = "dashed") +
   geom_density(aes(x = yday, y = after_stat(count), fill = year),  alpha = 0.5) +
   geom_density(data = mfsr_by_site, aes(x = yday, after_stat(count)), alpha = 0, linetype = "dashed") +
   facet_wrap(~stream, scales = "free_y", ncol = 1) +
