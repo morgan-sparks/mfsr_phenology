74b019a9bf256f9f88dc2382026dc7929e9e1984
Morgan.Sparks <morgan.sparks@morgans-mbp.boise.rmrs.fs.fed.us>
2025-03-21 08:44:46 -0600

    filter siegel_mfsr.RDS using COMID and write out to new RDS

--- R/01_siegel_mfsr_temps.R
+++ R/01_siegel_mfsr_temps.R
@@ -6,6 +6,10 @@
 #libraries
 library(fs); library(stringr); library(purrr); library(readr)
 
+
+# MFSR Filter -------------------------------------------------------------
+
+
 # file list of all files for salmon huc6 download
 huc_17062_files <- dir_ls("~/Downloads/st_pred_170602/")
 
@@ -28,8 +32,20 @@ huc_17062_files |>
   mutate(id = c(1:id_len)) |>  # numbers need to change if we use different files
   write_csv("./data/siegel_temperature/huc10s_ids_for_rds.csv")
 
-write_rds(mfsr_huc8, "./data/siegel_temperature/siegel_mfsr.rds")
+#write_rds(mfsr_huc8, "./data/siegel_temperature/siegel_mfsr.rds")
 
-#huc_17062_files |>
-#str_match(pattern = ("^1706020501.$"))
-             
+# Filter by COMID ---------------------------------------------------------
+
+#read in siegel mfsr filtered RDS
+siegel_mfsr <- readRDS(here::here("./data/siegel_temperature/siegel_mfsr.rds"))
+
+#read in spawn dataset with COMID
+spawn_cleaned_comid <- read_csv(here::here("./data/russ_spawn/mfsr_spawn_cleaned.csv"))
+
+#check to make sure spawn COMIDs are in siegel data (all TRUE, good)
+unique_comid <- unique(spawn_cleaned_comid$COMID)             
+
+siegel_mfsr_comid <- siegel_mfsr |> 
+  filter(COMID %in% unique_comid)
+
+#saveRDS(siegel_mfsr_comid, "./data/siegel_temperature/siegel_mfsr_comid.RDS")
