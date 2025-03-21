455c0954c614fa9804e40762559475592f577a54
Morgan.Sparks <morgan.sparks@morgans-mbp.boise.rmrs.fs.fed.us>
2025-03-21 09:29:09 -0600

    function for comid_temperature()

---
+++ R/comid_temperature.R
@@ -0,0 +1,59 @@
+library(dplyr); library(lubridate)
+
+
+comid_temperature <- function(temperature_data, 
+                              comid, 
+                              redd_id, 
+                              days, 
+                              date, 
+                              period = c("before, after, span")){
+  
+  dttm = mdy(date)
+  
+  if(period == "after"){
+    #days after
+    duration <- seq(dttm, dttm + (days -1), by = "days" )} else if (period == "before"){
+      #days before
+      duration <- seq(dttm - (days-1), dttm, by = "days" )} else if (period == "span"){
+        # days span
+        duration <-  seq (dttm -ceiling(days/2), dttm + floor(days/2), by = "days")}
+  
+  # calculate average temp
+  avg_temp <- data |> 
+    filter(COMID == comid) |>
+    mutate(spawn_date = ymd(tim.date)) |> 
+    filter(spawn_date %in% duration ) |> 
+    summarise(avg_temp = mean(prd.stream_temp))
+  
+  # out object
+  out <- tibble(spawn_date = mdy(date),
+                comid = comid,
+                redd_id = redd_id,
+                period = period,
+                duration = days,
+                avg_temp = avg_temp$avg_temp)
+  return(out)
+}
+
+
+
+# Example -----------------------------------------------------------------
+
+# temperature_data = readRDS(here::here("./data/siegel_temperature/siegel_mfsr_comid.RDS"))
+# 
+# comid = 23519529
+# 
+# redd_id = 1
+# 
+# date = "08312001"
+# 
+# days = 90 
+# 
+# period = "before"
+# 
+# comid_temperature(temperature_data = temperature_data,
+#            comid = comid,
+#            redd_id = redd_id,
+#           days = days,
+#           date = date,
+#           period = "before")
