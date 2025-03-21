f5e0b092d8ff213f17372cc1b15ccce33886c3ef
Morgan.Sparks <morgan.sparks@morgans-mbp.boise.rmrs.fs.fed.us>
2025-03-21 08:44:03 -0600

    clean mfsr_spawn but now with COMID in file
    write out file

--- R/03_spawn_data_clean.R
+++ R/03_spawn_data_clean.R
@@ -1,19 +1,22 @@
 library(tidyverse); library(ggridges)
 
-mfsr_spawn <- read.csv("./data/russ_spawn/mfsr_spawn_combined.csv")
+# now using Dona's COMID linked spawn data set
+mfsr_spawn <- read_csv(here::here("./data/russ_spawn/raw/MFSR_n4584_SnapWithCOMID_raw.csv"))
+
 
 #style not, all UPPER CASE are raw columns, lower case are appended columns
 
 # cleaned spawn data
 mfsr_spawn_cleaned <- mfsr_spawn |> 
-  drop_na(REACH) |>  #remove empty reaches
-  mutate(across(DATE, as.character)) |>  #make dates into character
-  mutate(date = mdy(gsub('.*^','0',DATE))) |> # add 0 to dates so we can use lubridate
+  #drop_na(REACH) |>  #remove empty reaches
+ # mutate(across(DATE, as.character)) |>  #make dates into character
+ # mutate(date = mdy(gsub('.*^','0',DATE))) |> # add 0 to dates so we can use lubridate
+  mutate(date = mdy(DATE)) |> 
   mutate(year = year(date)) |> # year column
   mutate(yday = yday(date)) |> # DOY column
   mutate(month = month(date)) |> # date column
-  filter(OBJ_CLASS %in% c("Redd", "REDD")) # filter only for redds
-
+  filter(OBJ_CLASS %in% c("Redd", "REDD")) |> # filter only for redds
+  filter(UNIQUE_ID != 3589)
 
 # plot to look at densities
 mfsr_spawn_cleaned |> 
@@ -42,7 +45,7 @@ mfsr_spawn_cleaned <- mfsr_spawn_cleaned |>
     REACH %in% c("LOON1", "LOON2", "LOON3") ~ "Loon",
     REACH %in% c("SULPHUR2") ~ 'Sulphur'))
 
-#write_csv(mfsr_spawn_cleaned, "./data/russ_spawn/mfsr_spawn_cleaned.csv")
+write_csv(mfsr_spawn_cleaned, "./data/russ_spawn/mfsr_spawn_cleaned.csv")
 
 
 
