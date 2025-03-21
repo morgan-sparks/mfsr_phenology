ff700675b5d4ac887ca093d2de63bb857dee5578
bmait101 <Bryan.Maitland@usda.gov>
2025-03-21 12:49:44 -0600

    Streamline fuction

--- R/comid_temperature.R
+++ R/comid_temperature.R
@@ -1,39 +1,41 @@
 library(tidyverse)
 
-
-comid_temperature <- function(temperature_data, 
-                              comid, 
-                              redd_id, 
-                              days, 
-                              date, 
-                              period = c("before, after, span")){
-  
-  #make data a dttm
-  dttm = mdy(date)
+comid_temperature <- function(temperature_data,
+                              comid, # redd dataset
+                              redd_id, # redd dataset
+                              spawn_date, # redd dataset
+                              day_spread,
+                              period = c("before, after, span")) {
+  # make data a dttm
+  dttm <- mdy(spawn_date)
   
   # make a seq of dates depending on period you want
-  if(period == "after"){
-    #days after
-    duration <- seq(dttm, dttm + (days -1), by = "days" )} else if (period == "before"){
-      #days before
-      duration <- seq(dttm - (days-1), dttm, by = "days" )} else if (period == "span"){
-        # days span
-        duration <-  seq (dttm -ceiling(days/2), dttm + floor(days/2), by = "days")}
+  if (period == "after") {
+    # days after
+    duration <- seq(dttm, dttm + (day_spread - 1), by = "days")
+  } else if (period == "before") {
+    # days before
+    duration <- seq(dttm - (day_spread - 1), dttm, by = "days")
+  } else if (period == "span") {
+    # days span
+    duration <- seq(dttm - ceiling(day_spread / 2), dttm + floor(day_spread / 2), by = "days")
+  }
   
   # calculate average temp
-  avg_temp <- temperature_data |> 
+  avg_temp <- temperature_data |>
     filter(COMID == comid) |>
-    mutate(spawn_date = ymd(tim.date)) |> 
-    filter(spawn_date %in% duration ) |> 
+    filter(tim.date %in% duration) |>
     summarise(avg_temp = mean(prd.stream_temp))
   
   # out object with all the goodies
-  out <- tibble(spawn_date = mdy(date),
-                comid = comid,
-                redd_id = redd_id,
-                period = period,
-                duration = days,
-                avg_temp = avg_temp$avg_temp)
+  out <- tibble(
+    spawn_date = mdy(spawn_date),
+    comid = comid,
+    redd_id = redd_id,
+    period = period,
+    duration = day_spread,
+    avg_temp = avg_temp$avg_temp
+  )
   return(out)
 }
 
@@ -47,15 +49,15 @@ comid_temperature <- function(temperature_data,
 # 
 # redd_id = 1
 # 
-# date = "08312001"
+# spawn_date = "08312001"
 # 
-# days = 90
+# day_spread = 90
 # 
-# period = "after"
+# period = "before"
 # 
 # comid_temperature(temperature_data = temperature_data,
-#            comid = comid,
-#            redd_id = redd_id,
-#           days = days,
-#           date = date,
-#           period = period)
+#                   comid = comid,
+#                   redd_id = redd_id,
+#                   day_spread = day_spread,
+#                   spawn_date = spawn_date,
+#                   period = period)
