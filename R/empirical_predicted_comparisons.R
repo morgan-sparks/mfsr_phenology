12a29b9d096a80213dc7037756a2d1a751662701
Morgan.Sparks <morgan.sparks@morgans-mbp.boise.rmrs.fs.fed.us>
2025-05-12 14:32:03 -0600

    add for Empirical vs. Predicted comparisons doc

--- R/empirical_predicted_comparisons.R
+++ R/empirical_predicted_comparisons.R
@@ -33,11 +33,20 @@ all_temps |>
   labs(x = "Empirical Temp", y ="Predicted Temp") +
   theme_bw()
 
+# all temps as one 
+all_temps |> 
+  ggplot(aes(x= DAILY_AVG_TEMP_C , y = prd.stream_temp)) +
+  geom_point(size = 0.15) +
+  geom_smooth(method = "lm", se = FALSE) +
+  geom_abline(intercept = 0, slope = 1) +
+  labs(x = "Empirical Temp", y ="Predicted Temp") +
+  theme_bw()
+
 
 
 ## Model fit ---------------------------------------------------------------
-
-mod<- lm(DAILY_AVG_TEMP_C ~ prd.stream_temp, data = all_temps)
+all_temps_mod <- all_temps |> mutate(month = as.factor(month(SampleDate)))
+mod<- lm(DAILY_AVG_TEMP_C ~ prd.stream_temp , data = all_temps_mod)
 
 sjPlot::tab_model(mod)
 
@@ -65,7 +74,50 @@ isaak_siegel |>
 is_summer <- isaak_siegel |> 
   mutate(date = mdy(SampleDate),
          month = month(date)) |>
-  filter(month %in% c(6:9))
+  filter(month %in% c(6:8))
 
 is_sum_mod <- lm(DailyMean~Stream_Temp, data = is_summer)
 sjPlot::tab_model(is_sum_mod)
+
+
+
+# compare russ reach data with gwynne + siegel data --------------------------------
+
+### get comids for sulphur 2 --------------------------------
+reach_comids <- read_csv(here::here("./data/russ_spawn/comids_in_reaches.csv"))
+
+sulphur2_comids <- reach_comids |> filter(REACH == "SULPHUR2") |> pull(COMID)
+
+### summarize reach temps --------------------------------
+
+reach_temps <- read_csv(here::here('./data/russ_spawn/raw/russ_reach_temps_02.csv'))
+
+reach_temps_sum <- reach_temps |> 
+  mutate(date_dttm = mdy_hm(Date),
+         date = date(date_dttm)) |> 
+  group_by(Stream, Reach, date) |> 
+  summarise(mean_temp = mean(Temperature))
+
+### filter to sulphur2
+sulphur2_reach_temps <- reach_temps_sum |> 
+  filter(Stream == "Sulphur" & Reach == 2)
+
+### gwynne temps sulphur2 2002--------------------------------
+
+sulphur2_gwynne <- empirical_temp |> 
+  filter(COMID %in% sulphur2_comids & 
+           SampleDate >= min(sulphur2_reach_temps$date) & 
+           SampleDate <= max(sulphur2_reach_temps$date))
+
+#no matches!
+
+### siegel temps sulphur2 2002 --------------------------------
+
+sulphur2_siegel <- siegel_temp |> 
+  filter(COMID %in% sulphur2_comids[6] & 
+           SampleDate >= min(sulphur2_reach_temps$date) & 
+           SampleDate <= max(sulphur2_reach_temps$date)) 
+
+sulphur2_mod <- lm(sulphur2_reach_temps$mean_temp ~ sulphur2_siegel$prd.stream_temp)
+summary(sulphur2_mod)
+
