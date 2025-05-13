3be3d0233f0ed40721e779247e6820a58eb4171e
Morgan.Sparks <morgan.sparks@morgans-mbp.boise.rmrs.fs.fed.us>
2025-05-13 10:14:49 -0600

    add comid_plots.R

---
+++ R/comid_plots.R
@@ -0,0 +1,141 @@
+library(tidyverse); library(ggridges); library(patchwork)
+
+
+mfsr_spawn_cleaned <- readRDS("./data/model_data_final.rds")
+
+
+# Good plots --------------------------------------------------------------
+
+#stream colors
+cols.streams <- c(
+  "Bear Valley" = "#55FF00",
+  "Beaver" = "#7A8EF5",
+  "Big" = "#FFFF00",
+  "Camus" = "#E64C00",
+  "Elk" = "#D7D79E",
+  "Loon" = "#7AF5CA",
+  "Marsh" = "#DF73FF",
+  "Sulphur" = "#FFAA00"
+)
+
+# redd count by elevation and stream for COMID
+p1 <- mfsr_spawn_cleaned |> 
+  group_by(stream, year, COMID, mean_elevation) |>
+  summarise(redd_count = n()) |> 
+  ggplot(aes(x = mean_elevation, y = stream, size = redd_count, fill = stream) ) +
+  geom_point(alpha = 0.5, shape = 21) +
+  lemon::facet_rep_wrap(~year, ncol = 1) +
+  scale_fill_manual(values = cols.streams) +
+  theme_classic() +
+  theme(legend.position = "none")
+
+# spawning distribution by COMID
+p2 <- mfsr_spawn_cleaned |> 
+  ggplot(aes(x= yday, y = stream, fill = stream, group = COMID, height = after_stat(density))) +
+  geom_density_ridges( stat = "density", scale =3, rel_min_height = .01, alpha = 0.65) +
+  lemon::facet_rep_wrap(~year, ncol=1) +
+  scale_fill_manual(values = cols.streams) +
+  theme_classic() +
+  theme(legend.position = "none")
+
+# plot two together
+p1 + p2
+
+
+
+# Bad plots ---------------------------------------------------------------
+
+
+
+mfsr_spawn_cleaned |>
+  ggplot(aes(x =yday, y = as.factor(year), color = COMID, fill = COMID, height = after_stat(density)))+
+  geom_density_ridges( stat = "density", scale = .95, rel_min_height = .01, alpha = 0.75,
+                       point_shape = '|', point_size = 3, point_alpha = 1) +
+  theme_classic() +
+  facet_wrap(~stream, ncol = 1) +
+  theme(legend.position = "none")
+
+mfsr_spawn_cleaned |> 
+  mutate(COMID = as.factor(COMID)) |> 
+  ggplot(aes(x = yday, y = stream, fill = as.factor(year), group = COMID))+
+  geom_boxplot() +
+  theme_classic() +
+  theme(legend.position = "none")
+
+
+mfsr_spawn_cleaned |>
+  mutate(COMID = as.factor(COMID),
+         year = as.factor(year)) |> 
+  filter(stream == "Bear Valley") |> 
+  ggplot(aes(x =yday, y = COMID, color = stream, fill = stream, height = after_stat(density)))+
+  geom_density_ridges( stat = "density", scale = .95, rel_min_height = .01, alpha = 0.85) +
+  facet_grid(stream~year, scales = "free_y" ) +
+  theme_classic() +
+  theme(legend.position = "none")
+
+
+mfsr_spawn_cleaned |> 
+  #filter(stream == "Big") |> 
+  ggplot(aes(x = yday, y = mean_elevation, fill = stream, group = COMID,height = after_stat(density))) +
+  geom_density_ridges( stat = "density", scale =15, rel_min_height = .01, alpha = 0.65) +
+  #facet_wrap(~year, ncol=1) +
+  facet_grid(year~stream ) +
+  theme_classic() 
+
+
+single_day_comids <- mfsr_spawn_cleaned |> 
+  #filter(stream == "Marsh" & year == 2004) |> 
+  group_by(year, COMID) |> 
+  summarise(yday_count = length(unique(yday))) |> 
+  filter(yday_count <= 1) 
+
+'%!in%' <- function(x,y)!('%in%'(x,y))
+
+mfsr_spawn_cleaned |>
+  filter(stream %in% c("Big", "Camas", "Loon", "Sulphur", "Bear Valley") & COMID %!in% c(single_day_comids$COMID)) |> 
+  ggplot(aes(x = yday, y = mean_elevation, fill = stream, group = COMID,height = after_stat(density))) +
+  geom_density_ridges( stat = "density", scale =15, rel_min_height = .01, alpha = 0.65) +
+  lemon::facet_rep_wrap(~year, ncol=1) +
+  #lemon::facet_rep_grid(year~stream ) +
+  theme_classic() 
+
+mfsr_spawn_cleaned |> 
+  
+  ggplot(aes(x = yday, y = mean_elevation, fill = stream, group = COMID)) +
+  geom_boxplot() +
+  #lemon::facet_rep_wrap(~year, ncol=1) +
+  lemon::facet_rep_grid(year~stream ) +
+  theme_classic()
+
+cols.streams <- c(
+  "Bear Valley" = "#55FF00",
+  "Beaver" = "#7A8EF5",
+  "Big" = "#FFFF00",
+  "Camus" = "#E64C00",
+  "Elk" = "#D7D79E",
+  "Loon" = "#7AF5CA",
+  "Marsh" = "#DF73FF",
+  "Sulphur" = "#FFAA00"
+)
+
+p1 <- mfsr_spawn_cleaned |> 
+  group_by(stream, year, COMID, mean_elevation) |>
+  summarise(redd_count = n()) |> 
+  ggplot(aes(x = mean_elevation, y = stream, size = redd_count, fill = stream) ) +
+  geom_point(alpha = 0.5, shape = 21) +
+  lemon::facet_rep_wrap(~year, ncol = 1) +
+  scale_fill_manual(values = cols.streams) +
+  theme_classic() +
+  theme(legend.position = "none")
+
+p2 <- mfsr_spawn_cleaned |> 
+  ggplot(aes(x= yday, y = stream, fill = stream, group = COMID, height = after_stat(density))) +
+  geom_density_ridges( stat = "density", scale =3, rel_min_height = .01, alpha = 0.65) +
+  lemon::facet_rep_wrap(~year, ncol=1) +
+  scale_fill_manual(values = cols.streams) +
+  theme_classic() +
+  theme(legend.position = "none")
+
+p1 + p2
+
+
